ff47b1d79ae91f75e45d910df06e533e1f3b96e0
gauravs <gauravshukla789@gmail.com>
2018-10-01 03:56:14 +0530

    added cnf conversion and clique constraint clauses

---
+++ CnfConversion.cpp
@@ -0,0 +1,381 @@
+/*
+ * CnfConversion.cpp
+ *
+ *  Created on: 30-Sep-2018
+ *      Author: gaurav
+ */
+
+/*
+ * expression.cpp
+ *
+ *  Created on: 07-Sep-2018
+ *      Author: gaurav
+ */
+#include <stack>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+class Expr
+{
+public:
+	int atom;
+	Expr* opd1;
+	Expr* opd2;
+	char oper;
+	Expr(){
+		this->atom = 0;
+		this->opd1 = NULL;
+		this->opd2 = NULL;
+		this->oper = '\0';
+	}
+	Expr(int atom){
+		this->atom = atom;
+		this->opd1 = NULL;
+		this->opd2 = NULL;
+		this->oper = '\0';
+	}
+	Expr(Expr* opd1,char oper){
+		this->atom = 0;
+		this->opd1 = opd1;
+		this->opd2 = NULL;
+		this->oper = oper;
+	}
+	Expr(Expr* opd1, Expr* opd2, char oper){
+		this->atom = 0;
+		this->opd1 = opd1;
+		this->opd2 = opd2;
+		this->oper = oper;
+	}
+	bool isAtomic(){
+		if(this->atom != 0 || (this->oper == '-' && this->opd1->atom !=0)){
+			return true;
+		}
+		return false;
+	}
+	bool isEmpty(){
+		if(this->atom == 0 && this->opd1 == NULL && this->opd2 == NULL && this->oper == '\0'){
+			return true;
+		}
+		return false;
+	}
+
+	static char* intToString(int x){
+		char* number = NULL;
+		if(x==0){
+			number = new char[10];
+			number[0]='\0';
+			return number;
+		}
+		number = intToString(x/10);
+		x = x%10;
+		char* digit= new char[2];
+		digit[0] = x+'0';
+		digit[1] = '\0';
+		strcat(number, digit);
+		//free Memory
+		delete[] digit;
+		digit = NULL;
+		return number;
+	}
+
+	char* getCNForm(){
+		char* result = NULL;
+		if(atom != 0){
+			return intToString(atom);
+		}
+		if(oper == '-'){
+			char* temp = opd1->getCNForm();
+			result = new char[500];
+			char op[2]={oper,'\0'};
+			result[0]= '\0';
+			strcat(result,op);
+			strcat(result,temp);
+			//free Memory
+			delete[] temp;
+			temp = NULL;
+		}
+		else if(oper != '\0') {
+			char* temp1= opd1->getCNForm();
+			char* temp2 = opd2->getCNForm();
+			result = new char[500];
+			char op[2] = {oper,'\0'};
+			result[0]= '\0';
+			strcat(result,temp1);
+			strcat(result,op);
+			strcat(result,temp2);
+			//free Memory
+			delete[] temp1;
+			temp1 = NULL;
+			delete[] temp2;
+			temp2 = NULL;
+		}
+		return result;
+
+	}
+};
+
+class CreateExpression{
+public:
+	static bool isdigit(char x){
+		if(x>='0' && x<='9'){
+			return true;
+		}
+		return false;
+	}
+	static bool isLeftAssociativeOper(char op){
+		switch(op){
+		case '+':
+		case '.': return true;
+		default : return false;
+		}
+	}
+	static bool isRightAssociativeOper(char op){
+		switch(op){
+		case '<':
+		case '=':
+		case '-': return true;
+		default : return false;
+		}
+
+	}
+	static int precedence(char op){
+		if(op == '-')
+			return 5;
+		else if(op == '.')
+			return 4;
+		else if(op == '+')
+			return 3;
+		else if(op == '<')
+			return 2;
+		else if(op == '=')
+			return 1;
+		else
+			return 0;
+	}
+	static void insert(char oper, stack<Expr*> &exp){
+		Expr *opd1=NULL,*opd2=NULL;
+		if(oper == '-'){
+			opd1 = exp.top();
+			exp.pop();
+			exp.push(new Expr(opd1,oper));
+		}
+		else{
+			opd2 = exp.top();
+			exp.pop();
+			opd1 = exp.top();
+			exp.pop();
+			exp.push(new Expr(opd1, opd2, oper));
+		}
+	}
+	static Expr* createExpr(char* infix){
+		stack<Expr*> exp;
+		stack<char> opr;
+		char oper;
+		for(int i=0;infix[i]!='\0';i++){
+
+			if(infix[i]==' ')
+				continue;
+			if(infix[i]=='('){
+				opr.push(infix[i]);
+			}
+			else if(isLeftAssociativeOper(infix[i])){
+				while(!opr.empty() && precedence(infix[i])<=precedence(opr.top())){
+					oper = opr.top();
+					opr.pop();
+					insert(oper,exp);
+				}
+				opr.push(infix[i]);
+			}
+			else if(isRightAssociativeOper(infix[i])){
+				while(!opr.empty() && precedence(infix[i])<precedence(opr.top())){
+					oper = opr.top();
+					opr.pop();
+					insert(oper,exp);
+				}
+				opr.push(infix[i]);
+			}
+			else if(infix[i]==')'){
+				while(!opr.empty() && (oper=opr.top())!='('){
+					opr.pop();
+					insert(oper,exp);
+				}
+				opr.pop();
+			}
+			else if(isdigit(infix[i])){
+				int val = 0;
+				while(infix[i]!='\0' &&  isdigit(infix[i])){
+					val = (val*10) + (infix[i]-'0');
+					i++;
+				}
+				i--;
+				exp.push(new Expr(val));
+			}
+		}
+		while(!opr.empty()){
+			char oper = opr.top();
+			opr.pop();
+			insert(oper,exp);
+		}
+		return exp.top();
+	}
+};
+
+class ConvertingRules{
+public:
+	static void applyRule(Expr** exprPTR){
+		Expr* expr = *exprPTR;
+		switch(expr->oper){
+		case '<':
+			expr->opd1 = new Expr(expr->opd1,'-');
+			expr->oper = '+';
+			break;
+		case '=':
+			expr->opd1 = new Expr(expr->opd1,new Expr(expr->opd2,'-'),'+');
+			expr->opd2 = new Expr(new Expr(expr->opd1,'-'),expr->opd2,'+');
+			expr->oper = '.';
+			break;
+		case '-':
+			Expr* opd = expr->opd1;
+			switch(opd->oper){
+			case '.':
+				expr->opd1 =new Expr(opd->opd1,'-');
+				expr->opd2 =new Expr(opd->opd2,'-');
+				expr->oper = '+';
+				break;
+			case '+':
+				expr->opd1 =new Expr(opd->opd1,'-');
+				expr->opd2 =new Expr(opd->opd2,'-');
+				expr->oper = '.';
+				break;
+			case '<':
+				expr->opd1 = opd->opd1;
+				expr->opd2 = new Expr(opd->opd2,'-');
+				expr->oper = '.';
+				break;
+			case '=':
+				expr->opd1 = new Expr(opd->opd1,opd->opd2,'+');
+				expr->opd2 = new Expr(new Expr(opd->opd1,'-'),new Expr(opd->opd2,'-'),'+');
+				expr->oper = '.';
+				break;
+			case '-':
+				//Memory Leaked.. ->will handle later
+				*exprPTR = opd->opd1;
+				break;
+			}
+		}
+		expr = NULL;
+	}
+
+	static void distributiveRule(Expr* expr){
+		if(expr!=NULL && expr->oper == '+'){
+			Expr* opd = expr->opd1;
+			if(opd->oper == '.'){
+				expr->opd1 = new Expr(opd->opd1,expr->opd2,'+');
+				expr->opd2 =new Expr(opd->opd2,expr->opd2,'+');
+				expr->oper = '.';
+				return;
+			}
+			opd = expr->opd2;
+			Expr* opd1 = expr->opd1;
+			if(opd->oper == '.'){
+				expr->opd1 = new Expr(opd1,opd->opd1,'+');
+				expr->opd2 =new Expr(opd1,opd->opd2,'+');
+				expr->oper = '.';
+				opd1 = NULL;
+				return;
+			}
+		}
+	}
+
+	//Top Down
+	static void CNForm(Expr** expressionPTR){
+		Expr* expression = *expressionPTR;
+		if(expression!=NULL){
+			applyRule(expressionPTR);
+		}
+		if(expression->opd1!=NULL){
+			CNForm(&expression->opd1);
+		}
+		if(expression->opd2!=NULL){
+			CNForm(&expression->opd2);
+		}
+		expression = NULL;
+	}
+
+	static void applyDistributiveRule(Expr* expr){
+		if(expr!=NULL){
+			distributiveRule(expr);
+		}
+		if(expr->opd1!=NULL){
+			applyDistributiveRule(expr->opd1);
+		}
+		if(expr->opd2!=NULL){
+			applyDistributiveRule(expr->opd2);
+		}
+	}
+
+	static vector<char*> tokenize(char* str, const char* delimiter){
+		vector<char*> tokens;
+
+		char* token = strtok((char*)str,delimiter);
+
+		while(token!=NULL){
+			tokens.push_back(token);
+			token = strtok(NULL,delimiter);
+		}
+		return tokens;
+	}
+	static void freeMemory(Expr* expression){
+		if(expression == NULL)
+			return;
+		if(expression->opd1 != NULL){
+			freeMemory(expression->opd1);
+		}
+		if(expression->opd2 != NULL){
+			freeMemory(expression->opd2);
+		}
+		if(expression->opd1 ==NULL && expression->opd2 ==NULL){
+			delete expression;
+			expression = NULL;
+		}
+	}
+
+	static char* replace(char* str, char old, char newOne)
+	{
+		char* res= new char[strlen(str)+1];
+		int i=0;
+		while(*str!='\0')
+		{
+			if(*str==old){
+				res[i]=newOne;
+			}
+			else{
+				res[i]=*str;
+			}
+			str++;
+			i++;
+		}
+		res[i]='\0';
+		return res;
+	}
+	static void getCNF(char* dnf, vector<char*>& clauses){
+		Expr* expression = CreateExpression::createExpr(dnf);
+
+		CNForm(&expression);
+		applyDistributiveRule(expression);
+		char* res = expression->getCNForm();
+		vector<char*> terms = tokenize(res,".");
+		for(unsigned int i=0;i<terms.size();i++){
+			clauses.push_back(replace(terms[i],'+',' '));
+		}
+
+		//Free Memory
+		delete[] res;
+		res= NULL;
+		//freeMemory(expression);
+	}
+};
+
+
+
--- DrugConnectSAT.cpp
+++ DrugConnectSAT.cpp
@@ -1,8 +1,7 @@
-#include <iostream>
-#include <cstring>
-#include <vector>
+#include "CnfConversion.cpp"
 #include <tr1/unordered_map>
-using namespace std;
+#include <fstream>
+#include<iostream>
 
 template <class T>
 struct comparator
@@ -31,29 +30,55 @@ typedef pair<int,int> iPair;
 
 class DrugConnect{
 public:
-	int n; //vertices count
-	int m; //edges count
-	int k; //cliques
+	unsigned int n; //vertices count
+	unsigned int m; //edges count
+	unsigned int k; //cliques
 	iPair* edges;
 	unorderedMap edgeMap;
 	vector<char*> cnf;
+	bool** adjacency;
 
-	void addVertexExistenceClauses();
-	void addEdgeExistenceClauses();
-	char* intToString(int);
-	char* edgeToString(iPair edge);
+	static char* edgeToString(iPair edge);
 	int VnCtoVr(int vertex,int component);
 	int EnCtoVr(iPair edge,int component);
 	pair<iPair,int> VrtoEnC(int);
 	iPair VrtoVnC(int);
-	DrugConnect(int n,iPair* edges,int m,int k){
+	void addVertexExistenceClauses();
+	void addEdgeExistenceClauses();
+	void addCliqueConstraint();
+	void addMutualExclusionConstraint();
+
+	DrugConnect(unsigned int n,iPair* edges,unsigned int m,unsigned int k){
 		this->n = n;
 		this->m = m;
 		this->k =k;
 		this->edges =edges;
-		//initialize edgeMap
-		for(unsigned int i=0;i<m;i++){
-			edgeMap.insert(make_pair(edgeToString(edges[i]), i));
+
+		//sort each edge(u,v) and initialize edgeMap
+		int temp;
+		unsigned int i;
+		for(i=0;i<m;i++){
+			if((this->edges)[i].first > (this->edges)[i].second){
+				temp = (this->edges)[i].first;
+				(this->edges)[i].first = (this->edges)[i].second;
+				(this->edges)[i].second = temp;
+			}
+			edgeMap.insert(make_pair(edgeToString((this->edges)[i]), i));
+		}
+
+		//initialize adjacency matrix
+		adjacency = new bool* [this->n];
+		for(i=0;i<this->n;i++){
+			adjacency[i] = new bool[this->n];
+			memset(adjacency[i], false, this->n*sizeof(bool));
+		}
+
+		pair<int,int> edge;
+		for(i=0;i<this->m;i++){
+			edge = edges[i];
+			adjacency[edge.first][edge.second] = true;
+			adjacency[edge.second][edge.first] = true;
+
 		}
 	}
 	~DrugConnect(){
@@ -64,88 +89,39 @@ public:
 			//tried making element NULL but failed..
 		}
 		//Delete Edges
-		delete[] edges;
-		edges = NULL;
+		if(edges!=NULL){
+			delete[] edges;
+			edges = NULL;
+		}
+
+		//Free cnf
+		unsigned int i;
+		for(i=0;i<cnf.size();i++){
+			delete[] cnf[i];
+			cnf[i] = NULL;
+		}
+		//Free Adjacency
+		for(i=0;i<this->n;i++){
+			delete[] adjacency[i];
+			adjacency[i] = NULL;
+		}
+		delete[] adjacency;
+		adjacency = NULL;
+
 	}
 };
-
-int DrugConnect::VnCtoVr(int vertex,int component){
-	return k * vertex + component + 1;
-}
-
-pair<int,int> DrugConnect::VrtoVnC(int variable){
-	
-	variable--;
-	int vertex = variable / k;
-	int component = variable % k;
-	return make_pair(vertex, component);
-}
-
-int DrugConnect::EnCtoVr(pair<int, int> edge, int component){
-	
-	char* edgeString = edgeToString(edge);
-	unsigned int edgeNum = edgeMap.find(edgeString);
-	return edgeNum * k + component + 1;
-}
-
-pair<pair<int,int>,int> DrugConnect::VrtoEnC(int variable){
-
-	int offset = k * n;
-	variable -= offset + 1;
-	int edgeNum  = variable / k;
-	int component = variable % k;
-	return make_pair(edges[edgeNum], component);
-}
-
-int main1(){
-	int n,m,k;
-	cin>>n>>m>>k;
-
-	iPair* edges = new iPair[m];
-	for(int i=0;i<m;i++){
-		cin>>edges[i].first;
-		cin>>edges[i].second;
-	}
-
-	DrugConnect agencies(n,edges,m,k);
-
-
-	edges = NULL;
-	return 0;
-}
-
-
-
-char* DrugConnect::intToString(int x){
-	char* number = NULL;
-	if(x==0){
-		number = new char[10];
-		number[0]='\0';
-		return number;
-	}
-	number = intToString(x/10);
-	x = x%10;
-	char* digit= new char[2];
-	digit[0] = x+'0';
-	digit[1] = '\0';
-	strcat(number, digit);
-	//free Memory
-	delete[] digit;
-	digit = NULL;
-	return number;
-}
 
 char* DrugConnect::edgeToString(iPair edge){
 
 	char* edgeStr = new char[22]; //(u,v) can be max of 22 size
-	char* val = intToString(edge.first);
+	char* val = Expr::Expr::intToString(edge.first);
 	strcpy(edgeStr,val);
 	strcat(edgeStr,",");
 
 	//Free Memory
 	delete[] val;
 
-	val= intToString(edge.second);
+	val= Expr::Expr::intToString(edge.second);
 	strcat(edgeStr,val);
 
 	//Free Memory
@@ -153,22 +129,56 @@ char* DrugConnect::edgeToString(iPair edge){
 
 	return edgeStr;
 }
+int DrugConnect::VnCtoVr(int vertex,int component){
+	return k * vertex + component + 1;
+}
+
+int DrugConnect::EnCtoVr(iPair edge, int component){
+
+	char* edgeString = edgeToString(edge);
+	unsigned int edgeNum = (edgeMap.find(edgeString))->second;
+	//Free Memory
+	delete[] edgeString;
+	edgeString = NULL;
+
+	return edgeNum * this->k + component + 1;
+}
+
+iPair DrugConnect::VrtoVnC(int variable){
+
+	variable--;
+	int vertex = variable / this->k;
+	int component = variable % this->k;
+	return make_pair(vertex, component);
+}
+
+pair<iPair,int> DrugConnect::VrtoEnC(int variable){
+
+	int offset = this->k * this->n;
+	variable -= offset + 1;
+	int edgeNum  = variable / this->k;
+	int component = variable % this->k;
+	return make_pair((this->edges)[edgeNum], component);
+}
+
 void DrugConnect::addVertexExistenceClauses(){
 	//max 10 digit variable allowed
 	char* var = NULL;
 	char* clause = NULL;
-	for(int vertex=0; vertex<this->n;vertex++){
+	unsigned int vertex,j;
+	for(vertex=0; vertex<this->n;vertex++){
 		clause = new char[11*this->k +1];
-		for(int j=0;j<this->k;j++){
-			var = intToString(VnCtoVr(vertex,j));
-			strcpy(clause,var);
-			strcat(clause," ");
+		strcpy(clause,"");
+		for(j=0;j<this->k;j++){
+			var = Expr::intToString(VnCtoVr(vertex,j));
+			strcat(clause,var);
+			if(j!=this->k-1)
+				strcat(clause," ");
 
 			//Free edgeVar
 			delete[] var;
 			var = NULL;
 		}
-		//clause will contain one extra space
 		cnf.push_back(clause);
 	}
 }
@@ -177,109 +187,128 @@ void DrugConnect::addEdgeExistenceClauses(){
 	char* edgeVar = NULL; //it is a variable
 	iPair edge;
 	char* clause = NULL;
-	for(int i=0;i<m;i++){
+	unsigned int i,j;
+	for(i=0;i<this->m;i++){
 		edge = (this->edges)[i];
 		clause = new char[11*this->k +1];//max 10 digit variable allowed
-		for(int j=0;j<k;j++){
-			edgeVar = intToString(EnCtoVr(edge,k));
-			strcpy(clause,edgeVar);
-			strcat(clause," ");
+		strcpy(clause, "");
+		for(j=0;j<this->k;j++){
+			edgeVar = Expr::intToString(EnCtoVr(edge,k));
+			strcat(clause,edgeVar);
+			if(j!=this->k-1)
+				strcat(clause," ");
 
 			//Free edgeVar
 			delete[] edgeVar;
 			edgeVar = NULL;
 		}
-		//clause will contain one extra space
 		cnf.push_back(clause);
 	}
 }
 
-int main(){
-	
-	int n,m,k;
-	cin>>n>>m>>k;
+void DrugConnect::addCliqueConstraint(){
+	unsigned int i,j,l,comp;
+	char* v1 =NULL;
+	char* v2 = NULL;
+	char* clause = NULL;
+	for(i=0 ;i<this->n; i++){
+		for(j=i+1;j< this->n;j++){
+			if(adjacency[i][j] == true){
+				//this edge must be present in atleast one clique
+				clause = new char[22*k+1];
+				strcpy(clause,"");
+				for(comp=0;comp<this->k;comp++){
+					v1 = Expr::intToString(VnCtoVr(i,comp));
+					v2 = Expr::intToString(VnCtoVr(j,comp));
+					//add cnf form....
+					strcat(clause, v1);
+					strcat(clause, ".");
+					strcat(clause, v2);
+					if(comp!=this->k-1)
+						strcat(clause, "+");
 
+					//Free Memory
+					delete[] v1;
+					delete[] v2;
+					v1= NULL;
+					v2 = NULL;
+				}
+				//convert clause to cnf form
+				vector<char*> clauses;
+				ConvertingRules::getCNF(clause,clauses);
+				for(l=0;l<clauses.size();l++){
+					cnf.push_back(clauses[l]);
+				}
+
+				//Free clause
+				delete[] clause;
+				clause = NULL;
+			}
+			else{
+				//~Edge -> ~(V1.V2)
+				for(comp=0;comp<this->k;comp++){
+					clause = new char[25];
+					v1 = Expr::intToString(VnCtoVr(i,comp));
+					v2 = Expr::intToString(VnCtoVr(j,comp));
+					strcpy(clause, "-");
+					strcat(clause, v1);
+					strcat(clause, " ");
+					strcat(clause, "-");
+					strcat(clause, v2);
+					cnf.push_back(clause);
+					//Free Memory
+					delete[] v1;
+					delete[] v2;
+					v1= NULL;
+					v2 = NULL;
+				}
+			}
+		}
+	}
+}
+
+void DrugConnect::addMutualExclusionConstraint(){
+
+}
+int main(int argc, char* argv[]){
+
+	ifstream fin("test.graph");
+	if(!fin){
+		cout<<"Error opening File. Exiting.";
+		return 1;
+	}
+	int n,m,k,x,y;
+	fin>>n>>m>>k;
+
+	//Assuming no repeated edges are there
 	iPair* edges = new iPair[m];
 	for(int i=0;i<m;i++){
-		cin>>edges[i].first;
-		cin>>edges[i].second;
+		fin>>x;
+		fin>>y;
+		edges[i] = make_pair(x-1,y-1);
 	}
+	//close the file
+	fin.close();
 
 	DrugConnect agencies(n,edges,m,k);
+	agencies.addVertexExistenceClauses();
+	agencies.addCliqueConstraint();
+	agencies.addMutualExclusionConstraint();
 
-	cout << "Number of variables = " << k*(n+m) << endl;
-	cout << "Map: graph to Variable:\n";
-	int v;
-	for(int i = 0; i <n; i++){
-		for(int j = 0; j< k; j++ ){
-			v = agencies.VnCtoVr(i , j);
-			cout << v <<" "; 
-		}
-		cout << endl;
+	//Write cnf into a file
+	ofstream fout("test.satinput");
+	if(!fout){
+		cout<<"Error opening File. Exiting.";
+		return 1;
 	}
-
-	cout << "VR to VnC\n";
-	for(int i = 0; i <m; i++){
-		for(int j = 0; j< k; j++ ){
-			v = agencies.EnCtoVr(edges[i] , j);
-			cout << v <<" "; 
-		}
-		cout << endl;
+	for(unsigned int i=0;i<(agencies.cnf).size();i++){
+		fout<<(agencies.cnf)[i]<<endl;
 	}
-
-	cout<<"Variable to Vertex \n";
-	for(int i = 1; i <= k*n; i++){
- 		pair<int,int> x = agencies.VrtoVnC(i); 
-		cout << i << " = " << x.first << "	"<< x.second << endl;
-	}
-
-	cout << "vari to Edge and component" << endl;
-	for(int i = k*n +1; i <= k*(n+m); i++){
- 		pair<pair<int,int>, int> x = agencies.VrtoEnC(i); 
-		cout << i << " = " << x.first.first << "	" << x.first.second << "	"<< x.second << endl;
-	}
-
-	// Test Code
-	// int adjacency[n][n];
-	// pair<int,int> edge;
-	// for(int i = 0; i < m; i++ ){
-	// 	edge = edges[i];
-	// 	adjacency[edge.first][edge.second] = 1;
-	// 	adjacency[edge.second][edge.first] = 1;
-	// }
-
-	// ofstream out;
-	// out.open("test1.satinput", ios::app);
-	// //V^V => E
-	// for(int i =0; i < k; i++){
-
-	// 	for(int j =0 ; j < n; j++){
-
-	// 		for(int l = j+1; l < n; l++){
-
-	// 			out << "-" << VnCtoVr(j, i) << " " << "-" << VnCtoVr(l, i);
-	// 			if(adjacency[j][l] == 1)
-	// 				out << " " <<EnCtoVr(make_pair(j, l), i);
-	// 			out << endl;
-	// 		}
-	// 	}
-	// }
-	// //V ^ ~V => ~E
-	// for(int i =0; i < k; i++){
-
-	// 	for(int j =0 ; j < n; j++){
-
-	// 		for(int l = j+1; l < n; l++){
-
-	// 			out << "-" << VnCtoVr(j, i) << " " << VnCtoVr(l, i);
-	// 			if(adjacency[j][l] == 1)
-	// 				out << " -" <<EnCtoVr(make_pair(j, l), i);
-	// 			out << endl;
-	// 		}
-	// 	}
-	// }
+	//close file
+	fout.close();
 
 	edges = NULL;
-	//out.close();
 	return 0;
 }
+
+
