4c67595a4ff87e32fa7b8c66b1e3ebaf163b072f
Gaurav shukla <gauravshukla789@gmail.com>
2018-10-02 07:05:44 +0530

    Added  Edge Variables and mutual exclusion constraint

--- outputProcessing.cpp
+++ outputProcessing.cpp
@@ -8,49 +8,55 @@
 #include<iostream>
 #include<fstream>
 #include<cstring>
+#include<sstream>
+#include<string>
 using namespace std;
 
-char* intToString(int x){
-	char* number = NULL;
-	if(x==0){
-		number = new char[10];
-		number[0]='\0';
-		return number;
-	}
-	number = intToString(x/10);
-	x = x%10;
-	char* digit= new char[2];
-	digit[0] = x+'0';
-	digit[1] = '\0';
-	strcat(number, digit);
-	//free Memory
-	delete[] digit;
-	digit = NULL;
-	return number;
+string intToString(int n){
+	stringstream ss;
+	ss<<n;
+	return ss.str();
 }
 
 int main(int argc, char* argv[]){
 
-	// getting File names
+	//Getting file names
+	if(!argv[1]){
+		cout<<"No file Given.";
+		return -1;
+	}
+
 	int len = strlen(argv[1]);
-	char inFilePath[len+10];
-	char outFilePath[len+10];
+
+	char inFilePath[len+15];
+	char outFilePath[len+15];
+	char graphFile[len+15];
+
 	strcpy(inFilePath,argv[1]);
 	strcat(inFilePath,".satoutput");
+
 	strcpy(outFilePath,argv[1]);
 	strcat(outFilePath,".subgraphs");
 
+	strcpy(graphFile,argv[1]);
+	strcat(graphFile,".graph");
+
+	//cout<<inFilePath<<" "<<outFilePath<<" "<<graphFile<<endl;
+
 	ifstream in;
 	in.open(inFilePath);
+	
 	ofstream out;
 	out.open(outFilePath);
-
+	
 	string s;
+
 	in >> s;
+	
 	int var;
 	int n, m, k;
-	ifstream graph;
-	graph.open("test.graph");
+	ifstream graph(graphFile);
+	
 	graph >> n >> m >> k;
 	bool truth[n][k];
 	if( s.compare("SAT") == 0){
@@ -58,7 +64,7 @@ int main(int argc, char* argv[]){
 		in >> var;
 		//cout << "Value of Var = " << var << endl;
 		for(int i =0; i < n; i++){
-			for(int j =0 ; j<k; j++){
+			for(int j =0 ; j< k; j++){
 				if(var > 0)
 					truth[i][j] = true;
 				else if(var < 0)
@@ -77,16 +83,18 @@ int main(int argc, char* argv[]){
 				if(truth[j][i]){
 					count++;
 					string s;
-					str.append(string(intToString(j+1)) + " ");
+					str += intToString(j+1) + " ";
 				}
 			}
 			if(count > 0){
 				out << "#" << i+1 << " " << count << endl;
+				str.erase(str.length()-1,1);
 				out << str << endl;
 			}
 		}
 	}
 	else out << 0;
+
 	out.close();
 	graph.close();
 	in.close();
